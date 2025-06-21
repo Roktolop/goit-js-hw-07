f402b6cffce3ec7c7b2b417e4f3705a60d4daa80
Volodymyr <sanin.vova200@gmail.com>
2025-06-22 01:02:46 +0300

    fix task-3

--- js/task-3.js
+++ js/task-3.js
@@ -4,5 +4,5 @@ const textInput = document.querySelector(`#name-input`);
 const output = document.querySelector(`#name-output`)
 
 textInput.addEventListener(`input`, (event) => {
-    output.textContent = event.currentTarget.value;
+    output.textContent = event.currentTarget.value.trim();
 });
