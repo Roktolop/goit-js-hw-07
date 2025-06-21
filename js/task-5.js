899c67a6cfaa5266997620d174b66352c3827b68
Volodymyr <sanin.vova200@gmail.com>
2025-06-21 20:31:24 +0300

    make task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widget = document.querySelector(`.widget`);
+const button = document.querySelector(`.change-color`)
+const color = document.querySelector(`.color`);
+button.addEventListener("click", changeColor);
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  widget.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+}
